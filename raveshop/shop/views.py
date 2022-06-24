c10d3dde07f0b55bde13b51bca4506c75449dcfa
Sviatoslav <slaviktkachuk11@gmail.com>
2022-06-24 09:20:11 +0300

    templates

--- raveshop/shop/views.py
+++ raveshop/shop/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    print(request)
+    return render(request, 'shop/index.html')
+
+
+def basket(request):
+    print(request)
+    return HttpResponse('Basket')
