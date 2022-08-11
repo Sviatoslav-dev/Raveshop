aee5c7f860d6af810678b1b5ea027b7cd0b14626
sviatoslav-dev <slaviktkachuk11@gmail.com>
2022-08-11 14:54:35 +0300

    env

--- raveshop/raveshop/settings.py
+++ raveshop/raveshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default=None) # env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,4 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-BOT_TOKEN = env('BOT_TOKEN')
+BOT_TOKEN = os.getenv('BOT_TOKEN', default=None) #env('BOT_TOKEN')
